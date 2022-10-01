f67e5ebcf468fc39de2fc2f95bd76745dc086e9d
ramirososa <53145455+ramirososa@users.noreply.github.com>
2022-10-01 20:09:36 -0300

    Created main & heading file

---
+++ proyecto/main/main.h
@@ -0,0 +1,29 @@
+//THIS IS THE HEADER OF THE PROJECT ALL DE DEFINITIONS WILL BE INCLUDED HERE
+
+//ALL OF THE OUTPUTS
+#define s1 = 1;
+#define s2 = 1;
+#define s3 = 1;
+#define s4 = 1;
+#define s5 = 1;
+#define s6 = 1;
+#define s7 = 1;
+#define s8 = 1;
+#define s9 = 1;
+#define s10 = 1;
+#define s11 = 1;
+#define s12 = 1;
+
+//ALL OF THE INPUTS
+#define e1 = 1;
+#define e2 = 1;
+#define e3 = 1;
+#define e4 = 1;
+#define e5 = 1;
+#define e6 = 1;
+#define e7 = 1;
+#define e8 = 1;
+#define e9 = 1;
+#define e10 = 1;
+#define e11 = 1;
+#define e12 = 1;
---
+++ proyecto/main/main.ino
@@ -0,0 +1,13 @@
+/*ARCHIVO MAIN DEL PROYECTO*/
+#include main.h
+
+
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(9600); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
