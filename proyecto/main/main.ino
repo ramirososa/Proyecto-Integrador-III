a58c19b84814579345e873a4f863a4b0bcde6ef6
ramirososa <53145455+ramirososa@users.noreply.github.com>
2022-10-23 18:40:34 -0300

    Reestructura pooling y logica

--- proyecto/main/main.ino
+++ proyecto/main/main.ino
@@ -3,13 +3,29 @@
 
 const int PINOUT_INPUTS = [E1,E2,E3,E4,E5,E6,E7,E8,E9,E10,E11,E12];
 const int PINOUT_OUTPUTS = [S1,S2,S3,S4,S5,S6,S7,S8,S9,S10,S11,S12];
-int INPUTS_ENABLE = [];
+int INPUTS_ENABLE = [1,0,0,0,0,0,0,0,0,0,0,0];
+
 // Hacer una estructura con por un lado el numero de pines y por otro lado si el pin se prendio.
 
-boolean FLAG_ESCLUSA = false;
-boolean FLAG_RECEIVE = false;
-boolean FLAG_BT = false;
+bool FLAG_ESCLUSA = false;
+bool FLAG_RECEIVE = false;
+bool FLAG_SENSOR = false;
+bool FLAG_BT = false;
+bool FLAG_ETH = false;
+
+char STATE_ETH;
 char STATE_BT;
+char STATE;
+
+//Texto 
+const char[] OFF = "APAGADA";
+const char[] AUTOMATIC = "AUTOMATICA";
+const char[] ONLYLEAVE = "SOLOSALIR";
+const char[] OPEN = "ABIERTA";
+const char[] NIGHT = "NOCHE";
+const char[] PANIC = "PANICO";
+const char[] ESCLUSA = "ESCLUSA"
+
 
 void isr_esclusa(){
   FLAG_ESCLUSA = !FLAG_ESCLUSA; //Pongo el flag de esclusa prendida o apagada en el opuesto, por defecto empiezo el programa con esclusa apagada
@@ -63,36 +79,96 @@ void mode_esclusa(){
   
   
 }
+// A que le damos prioridad si llegan las tres juntas el BT el ETH o un btn??
+void change_state(){
+  bool need_out=true
+  int pinout;
+  if(FLAG_BT){
+    switch(STATE_BT){
+      case OFF:
+        STATE = OFF;
+        pinout = PINOUT_OUTPUTS[x];
+      break;
+      case AUTOMATIC:
+        STATE = AUTOMATIC;
+        pinout = PINOUT_OUTPUTS[x];
+      break;
+      case ONLYLEAVE:
+        STATE = ONLYLEAVE;
+        pinout = PINOUT_OUTPUTS[10];
+      break;
+      case OPEN:
+        STATE = OPEN;
+        pinout = PINOUT_OUTPUTS[9];
+      break;
+      case NIGHT:
+        STATE = NIGHT;
+        pinout = PINOUT_OUTPUTS[8];
+      break;
+      case PANIC:
+        STATE = PANIC;
+        pinout = PINOUT_OUTPUTS[7];
+      break;
+      case ESCLUSA:
+        STATE = ESCLUSA;
+        pinout = PINOUT_OUTPUTS[x];
+        need_out=false;
+      break;      
+    }
+  }  
+  if(FLAG_ETH){
+    
+  }
+  if(FLAG_STATE){
+    
+  }
+  output_cs(pinout, need_out);
+}
+
+
+
+
+void output_cs(int pin, bool flag){
+  //PUBLISH DEL ESTADO por bt y eth, transmitir a slave??
+  
+}
+
+void output_sens(int pin){
+  
+}
 
 void loop() {
-  
-  INPUTS_ENABLE = [];
+ 
+  INPUTS_ENABLE = [1,0,0,0,0,0,0,0,0,0,0,0];
   
   while(1){
 /*----------------- Analisis de flancos -----------------*/
-    int j=0;
-    if(digitalRead(PINOUT_INPUTS[0])==LOW){
-        ins[j]=PINOUT_INPUTS[0];
+    int i=0;
+    if(digitalRead(PINOUT_INPUTS[i])==LOW){
+        INPUTS_ENABLE[i]=0;
         FLAG_RECEIVE=true;
-        j++;
+        i++;
       }
-    int i=1;
     while(i<12){
       if(digitalRead(PINOUT_INPUTS[i])==HIGH){
-        ins[j]=PINOUT_INPUTS[i];
+        INPUTS_ENABLE[i]=1;
         FLAG_RECEIVE=true;
-        j++;
+        i++;
+        if(i>1 && i<8){
+          FLAG_SENSOR = true;//levanto flag para chequear que hago con el sensor.  
+        }
       }
     }
-    if(Serial1.available()>0){
-      STATE_BT = Serial1.read();        
+    if(Serial2.available()>0){
+      STATE_BT = Serial2.read();
+      FLAG_BT = true;      
     }
+    
 /*----------------- Llamar funciones auxiliares -----------------*/
-    if(!FLAG_ESCLUSA && FLAG_RECEIVE) mode_no_esclusa();
-    else if(FLAG_ESCLUSA && FLAG_RECEIVE) mode_esclusa();
-    if(STATE_BT!=0){
-      read_BT()
-    }
+    if(FLAG_SENSOR) sensor();
+    if(FLAG_STATE || FLAG_BT || FLAG_ETH) change_state();
+    
+    
   }
   
 
